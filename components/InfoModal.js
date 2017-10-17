89b5eafe5b74220f55736e1db0000ecf21ae9779
Alexandra Roth <alexandra.roth@vaynermedia.com>
2017-10-17 17:41:50 -0400

    links working need to be styled

--- components/InfoModal.js
+++ components/InfoModal.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
 
 //----native elements
-import { StyleSheet, Text, View, Button, Modal, Image, resizeMode, ScrollView, Linking } from 'react-native';
+import { StyleSheet, Text, View, Modal, Image, resizeMode, ScrollView, Linking, TouchableHighlight } from 'react-native';
 
 //----libraries
 import Hyperlink from 'react-native-hyperlink'
 
+links = [
+    'https://www.brewdog.com/welcome',
+    'https://www.brewdog.com/brewery/brewery',
+    'https://www.brewdog.com/brewery/usa-brewery'
+]
+
 export default class InfoModal extends Component {
+    constructor() {
+        super()
+    }
+    handlePress = (i) => {
+        Linking.openURL(links[i]);
+    };
     render() {
         return(
                 <View style={styles.modal}>
@@ -15,36 +27,19 @@ export default class InfoModal extends Component {
                         source={ require('../images/ohiobrewery.png')}
                         resizeMode="contain"
                     />
-                    <View style={styles.content1}>
-                        <Text style={styles.name}>Useful Links</Text>
-                    </View>
-                    <ScrollView style={styles.content}>
-                        <Hyperlink
-                            linkStyle={ { color: 'blue' } }
-                            linkText={ url => url === 'https://www.brewdog.com/brewery/brewery' ? 'Ellon, Aberdeenshire' : url }>
+                    <Text>Brewery HQ</Text>
+                    <TouchableHighlight onPress={() => {this.handlePress(2)}}>
+                        <Text>Ellon, Aberdeenshire</Text>
+                    </TouchableHighlight>
+                    <Text>US Brewery</Text>
+                    <TouchableHighlight onPress={() => {this.handlePress(3)}}>
+                        <Text>Columbus, OH</Text>
+                    </TouchableHighlight>
+                    <Text>Website Home</Text>
+                    <TouchableHighlight onPress={() => {this.handlePress(1)}}>
+                        <Text>brewdog.com</Text>
+                    </TouchableHighlight>
 
-                            <Text>Brewery HQ</Text>
-                            <Text style={styles.link}>
-                                https://www.brewdog.com/brewery/brewery
-                            </Text>
-                        </Hyperlink>
-                        <Hyperlink
-                            linkStyle={ { color: 'blue' } }
-                            linkText={ url => url === 'https://www.brewdog.com/brewery/usa-brewery' ? 'Columbus, Ohio' : url }>
-                            <Text>{"\n"}US Brewery</Text>
-                            <Text style={styles.link}>
-                                https://www.brewdog.com/brewery/usa-brewery
-                            </Text>
-                        </Hyperlink>
-                        <Hyperlink
-                            linkStyle={ { color: 'blue' } }
-                            linkText={ url => url === 'https://www.brewdog.com/welcome' ? 'Website Home' : url }>
-                            <Text>{"\n"}Web Home</Text>
-                            <Text style={styles.link}>
-                                https://www.brewdog.com/welcome
-                            </Text>
-                        </Hyperlink>
-                    </ScrollView>
                 </View>
 
         );
@@ -79,3 +74,34 @@ const styles = StyleSheet.create({
         paddingTop: 20,
     },
 });
+
+// <View style={styles.content1}>
+//     <Text style={styles.name}>Useful Links</Text>
+// </View>
+// <ScrollView style={styles.content}>
+//     <Hyperlink
+//         linkStyle={ { color: 'blue' } }
+//         linkText={ url => url === 'https://www.brewdog.com/brewery/brewery' ? 'Ellon, Aberdeenshire' : url }>
+//
+//         <Text>Brewery HQ</Text>
+//         <Text style={styles.link}>
+//             https://www.brewdog.com/brewery/brewery
+//         </Text>
+//     </Hyperlink>
+//     <Hyperlink
+//         linkStyle={ { color: 'blue' } }
+//         linkText={ url => url === 'https://www.brewdog.com/brewery/usa-brewery' ? 'Columbus, Ohio' : url }>
+//         <Text>{"\n"}US Brewery</Text>
+//         <Text style={styles.link}>
+//             https://www.brewdog.com/brewery/usa-brewery
+//         </Text>
+//     </Hyperlink>
+//     <Hyperlink
+//         linkStyle={ { color: 'blue' } }
+//         linkText={ url => url === 'https://www.brewdog.com/welcome' ? 'Website Home' : url }>
+//         <Text>{"\n"}Web Home</Text>
+//         <Text style={styles.link}>
+//             https://www.brewdog.com/welcome
+//         </Text>
+//     </Hyperlink>
+// </ScrollView>
